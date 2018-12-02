47ad9e1ed966943b0489abd21b43a0e2f0988354
vickeykm <45536117+vickeykm@users.noreply.github.com>
2018-12-02 19:27:31 +0530

    Initial Commit

---
+++ src/main/webapp/scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/* see http://www.jshint.com/docs/ if you want to know what this is: */
+/* global Pages */
+
+// Where do all these partials live?
+var TemplatePrefix = 'views/';
+
+// Create Angular application module angularjsSimpleWebsiteApp.
+// Declare ngRoute as a dependency. http://docs.angularjs.org/api/ngRoute
+// Then configure the $routeProvider by defining the routes.
+
+angular.module('angularjsSimpleWebsiteApp', ['ngRoute'])
+
+  .config(function ($routeProvider) {
+    // register the routes and the templates
+    // http://docs.angularjs.org/api/ngRoute.$routeProvider
+    for ( var path in Pages ) {
+      var template = Pages[path][0];
+      $routeProvider.when( path, {templateUrl: TemplatePrefix + template});
+    }
+    // the default route
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
+
+/* jshint unused:false */
+
+// The NavigationLinks controller.
+function NavigationLinks( $scope, $location ) {
+  // $routeChangeSuccess is an event that is fired,
+  // when the app has switched from one route to another.
+  // http://docs.angularjs.org/api/ngRoute.$route
+  // here we subscribe to that event. 
+  $scope.$on('$routeChangeSuccess',
+    function() {
+      $scope.locationPath = $location.path();
+      console.log('locationPath: ' + $location.path() );
+    }
+  );
+}
